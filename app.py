a52515bc40aceab6a986a9cb0e53c10cd05f3c5e
Devang singh <devangkumarprajapati1908@gmail.com>
2026-06-28 04:14:41 +0000

    Enhance app.py by adding additional imports and configuring Streamlit page settings

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from sklearn.preprocessing import LabelEncoder
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # -------------------------------
-# 
+# Theme, layout, sidebar
+# --------------------------------
+st.set_page_config(
+    page_title="Coffee Sales Performance Dashboard",
+    page_icon="☕",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
